78cc921c0039d7a72932e99934c24285dba33377
Shubham <shubhamchavan7972@gmail.com>
2025-01-29 19:41:59 +0530

    all commit

---
+++ First.java
@@ -0,0 +1,14 @@
+// class First{
+//     public static void main(String[] args) {
+//         System.out.println("Hello Shubham");
+//     }
+    
+// }
+
+
+
+class nilesh{
+    public static void main(String args[]){
+        System.out.println("Hello Nilesh Borase You have been shortlisted for upcoming interview!!");
+    }
+}
